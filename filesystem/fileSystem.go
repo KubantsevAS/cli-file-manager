67de86803db042739071ce9f644bf060a15f5723
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-11-18 00:10:20 +0300

    feat: add list command

--- filesystem/fileSystem.go
+++ filesystem/fileSystem.go
@@ -8,7 +8,7 @@ func NewLocalFS() *LocalFS {
 	return &LocalFS{}
 }
 
-func (fs *LocalFS) ChangeDirectory() error {
+func (fs *LocalFS) ChangeDirectory(path string) error {
 	return nil
 }
 
@@ -32,6 +32,6 @@ func (fs *LocalFS) Move(src, dst string) error {
 	return nil
 }
 
-func (fs *LocalFS) Delete(src, dst string) error {
+func (fs *LocalFS) Delete(path string) error {
 	return nil
 }
